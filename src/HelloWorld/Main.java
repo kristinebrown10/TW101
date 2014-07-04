2a80531706fd732172ca67474c8e7e09d25eac57
Kristine Brown <kristinebrown10@gmail.com>
2014-07-03 19:36:22 -0700

    Hello World

---
+++ src/HelloWorld/Main.java
@@ -0,0 +1,10 @@
+package HelloWorld;
+
+/**
+ * Created by KBrown on 7/3/14.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
