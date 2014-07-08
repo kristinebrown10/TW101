e74ad3e42a9f7ae2a3b682e8926ae2c249476899
Kristine Brown <kristinebrown10@gmail.com>
2014-07-07 19:32:25 -0700

    Ch 6 complete

--- src/Ch6/GameLauncher.java
+++ src/Ch6/GameLauncher.java
@@ -1,11 +1,27 @@
 package Ch6;
-
+import java.util.ArrayList;
 /**
  * Created by KBrown on 7/3/14.
  */
 public class GameLauncher {
     public static void main (String[] args) {
         GuessingGame gg = new GuessingGame();
-        gg.startGame();
+        GameHelper helper = new GameHelper();
+        int randomNum = (int) (Math.random() * 100);
+        ArrayList<String> userGuesses = new ArrayList<String>();
+
+        gg.setRandomNum(randomNum);
+        boolean isAlive = true;
+
+        while(isAlive) {
+            String guess = helper.getUserInput("Enter your guess");
+            String result = gg.checkYourself(guess);
+            userGuesses.add(guess);
+            if(result.equals("win")) {
+                isAlive = false;
+                System.out.println("You win!");
+                System.out.print("You guessed: " + userGuesses.toString());
+            }
+        }
     }
 }
